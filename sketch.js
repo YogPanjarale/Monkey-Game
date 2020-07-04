ce0968f67a59e8bdbd5f64a151e13d935378651f
YogPanjarale <64301340+YogPanjarale@users.noreply.github.com>
2020-07-04 10:04:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var play = 1;
 var end = 0;
 var GameState = play;
 //Global Variables
-var _background, ground, back_img, monkey, running_img, banana_img, obstacle_img, obstacleGroup, foodGroup, score, hit;
+var _background, ground, back_img, monkey, running_img, banana_img, obstacle_img, obstacleGroup, foodGroup, score, hit,restart_img,GameOver_img,restart,GameOver,depth=0,speed=8;
 
 
 function preload() {
@@ -10,7 +10,8 @@ function preload() {
   running_img = loadAnimation("Monkey_01.png", "Monkey_02.png", "Monkey_03.png", "Monkey_04.png", "Monkey_05.png", "Monkey_06.png", "Monkey_07.png", "Monkey_08.png", "Monkey_09.png", "Monkey_10.png");
   obstacle_img = loadImage("stone.png");
   banana_img = loadImage("Banana.png");
-
+restart_img = loadImage("restart.png");
+  GameOver_img = loadImage("gameOver.png");
 }
 
 function setup() {
@@ -21,7 +22,7 @@ function setup() {
   _background = createSprite(width, 25);
   _background.addImage(back_img);
   _background.scale = 1.3;
-  _background.velocityX = -6;
+  _background.velocityX = -speed;
 
   ground = createSprite(width / 2, 270, width, 50);
   ground.visible = false
@@ -29,6 +30,14 @@ function setup() {
   monkey = createSprite(100, 200);
   monkey.addAnimation("running", running_img);
   monkey.scale = 0.12;
+  
+   restart = createSprite(width / 2, height / 1.5);
+  restart.addImage("restart", restart_img);
+  restart.visible = false;
+
+  gameover = createSprite(width / 2, height / 3);
+  gameover.addImage("go", GameOver_img);
+  gameover.visible = false;
 
   score = 0;
   obstaclesGroup = new Group();
@@ -55,8 +64,8 @@ function draw() {
       hit -= 1;
       if (hit >= 1) {
         monkey.scale = 0.1;
-      }
-      if (hit == 0) {
+      }console.log(hit    )
+      if (hit <= 0) {
         GameState = end
       }
     }
@@ -67,7 +76,22 @@ function draw() {
   }
 
   if (GameState == end) {
-    monkey.setVelocity(0, 0);
+    monkey.setVelocity(0,0);
+    ground.velocityX = 0;
+    _background.velocityX = 0
+    obstaclesGroup.setVelocityXEach(0);
+    obstaclesGroup.setLifetimeEach(-1);
+    foodGroup.setVelocityXEach(0);
+    foodGroup.setLifetimeEach(-1);
+    //trex.changeAnimation("collided", trex_collided);
+    restart.visible = true;
+    gameover.visible = true;
+     monkey.pause();
+    restart.depth = depth + 1;
+    if (mousePressedOver(restart)) {
+      reset();
+    }
+   
      
   }
   switch (score) {
@@ -90,7 +114,18 @@ function draw() {
   scoreboard();
 }
 
-function reset() {}
+function reset() {
+  
+  obstaclesGroup.destroyEach();
+  foodGroup.destroyEach();
+  _background.velocityX = -speed;
+  restart.visible = false;
+  gameover.visible = false;
+  score = 0;
+  hit= 0;
+  GameState = play;
+  monkey.play();
+}
 
 function spawnStones() {
   if (frameCount % 180 == 0 && random(1, 5) >= 2) {
@@ -102,7 +137,7 @@ function spawnStones() {
     obstaclesGroup.add(stone)
     //stone.debug = true 
     stone.setCollider("rectangle", -60, 35, 200, 400, 30);
-  }
+  }depth++;
 
 }
 
